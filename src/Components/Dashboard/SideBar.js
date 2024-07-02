41b9fd0581cea1262d8172acbb93af387def6a14
walid <walidashraf050@gmail.com>
2024-07-02 19:43:38 +0300

    WindowContext & Small Screens Side Bar

--- src/Components/Dashboard/SideBar.js
+++ src/Components/Dashboard/SideBar.js
@@ -4,18 +4,24 @@ import { faUsers } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
 import { Menu } from "../../Context/MenuContext";
 import { useContext } from "react";
+import { WindowSize } from "../../Context/WindowContext";
 
 
 export default function SideBar() {
 
     const menu = useContext(Menu);
+    const WindowContext = useContext(WindowSize);
+    const windowSize = WindowContext.windowSize;
+    // console.log(windowSize.windowSize);
     const isOpen = menu.isOpen;
     // console.log(isOpen);
     // console.log(menu);
 
     return (
         <>
-        <div className="side-bar pt-3" style={{ width: isOpen? "240px": "fit-content"}}>
+        <div className="side-bar pt-3" style={{ 
+            left: windowSize < '768' ? (isOpen ? 0: "-100%"): '',
+            width: isOpen? "240px": "fit-content"}}>
             <NavLink to={"users"} className="d-flex align-items-center gap-2 side-bar-link">
             <FontAwesomeIcon icon={faUsers} style={{ padding: isOpen? "10px 8px 10px 15px": "10px 14px" }}/>
             <p className="m-0" style={{
--- src/index.js
+++ src/index.js
@@ -6,19 +6,22 @@ import "./Css/components/button.css";
 import "./Css/components/alerts.css";
 import "./Css/components/google.css";
 import "./Pages/Auth/Auth.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import MenuContext from "./Context/MenuContext";
+import WindowContext from "./Context/WindowContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MenuContext>
-      <Router>
-      <App />
-    </Router>
-    </MenuContext>
+    <WindowContext>
+      <MenuContext>
+        <Router>
+          <App />
+        </Router>
+      </MenuContext>
+    </WindowContext>
   </React.StrictMode>
 );
